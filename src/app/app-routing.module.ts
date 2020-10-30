c9a2f89b98bbe3c33bdacfced756861903ff68d9
Renan Reboredo <renanrreboredo@gmail.com>
2020-10-30 15:09:22 -0300

    Init generation list component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { GenerationsComponent } from './generations/generations.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: GenerationsComponent,
+    path: 'generations',
+  },
+];
 
 @NgModule({
+  exports: [RouterModule],
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

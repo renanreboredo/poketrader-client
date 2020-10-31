0fe0ee31d87ec4ed72007eaa9f2766596ebdfd90
Renan Reboredo <renanrreboredo@gmail.com>
2020-10-31 02:38:07 -0300

    Allows to remove Pokemons from trade

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../auth.service';
-import { PoketraderApiService } from '../poketrader-api.service';
+import { Pokemon, PoketraderApiService } from '../poketrader-api.service';
 
 @Component({
   selector: 'app-pokemon-list',
@@ -45,7 +45,7 @@ export class PokemonListComponent implements OnInit {
   trade() {
     const userID = this.authService.currentUserValue.userID;
     this.pokeapiService.trade(this.selectedPokemonsP1, this.selectedPokemonsP2, userID).subscribe((res: any) => {
-      if (res.message === 'Success') {
+      if (res.data !== false) {
         this.router.navigateByUrl('history');
       }
     });
@@ -54,7 +54,28 @@ export class PokemonListComponent implements OnInit {
   checkTrade() {
     const userID = this.authService.currentUserValue.userID;
     this.pokeapiService.isFairTrade(this.selectedPokemonsP1, this.selectedPokemonsP2).subscribe((res: any) => {
-      this.isFair = res.message === 'Success';
+      this.isFair = res.data;
     });
+  }
+
+  removePokemonFromTrade(pokemon: Pokemon, player) {
+    if (player === 1) {
+      this.selectedPokemonsP1 = this.removePokemonFromArray(this.selectedPokemonsP1, pokemon);
+    } else {
+      this.selectedPokemonsP2 = this.removePokemonFromArray(this.selectedPokemonsP2, pokemon);
+    }
+  }
+
+  removePokemonFromArray(pokemons: any[], pokemonRemoved) {
+    let found = false;
+    const removedPokemonArr = pokemons.filter(item => {
+      if (pokemonRemoved.name === item.name && !found) {
+        found = true;
+        return false;
+      }
+      return pokemonRemoved.name === item.name && found;
+    });
+    const remainderPokemonArr = pokemons.filter(pkmn => pokemonRemoved.name !== pkmn.name);
+    return [ ...removedPokemonArr, ...remainderPokemonArr ];
   }
 }
